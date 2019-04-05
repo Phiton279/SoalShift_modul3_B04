53dc4b57a0b15957a250d5af2d48fcdba3347a18
Ersad Ishlahuddin <36990076+ersadishla@users.noreply.github.com>
2019-04-05 21:37:59 +0700

    Update soal4.c

--- soal4/soal4.c
+++ soal4/soal4.c
@@ -9,61 +9,59 @@
 pthread_t tid[2];
 
 
-void* makeDir1(void* arg){
+void* makeDir1(void* nothing){
 	system("mkdir ~/Documents/FolderProses1");
 }
-void* makeDir2(void* arg){
+void* makeDir2(void* nothing){
 	system("mkdir ~/Documents/FolderProses2");
 }
 
-void* storeList1(void *arg){
+void* storeList1(void *nothing){
 	system("ps -aux | head -11 | tail -10 > ~/Documents/FolderProses1/SimpanProses1.txt");
 }
-void* storeList2(void *arg){
+void* storeList2(void *nothing){
 	system("ps -aux | head -11 | tail -10 > ~/Documents/FolderProses2/SimpanProses2.txt");
 }
 
-void* compress1(void *arg){
+void* compress1(void *nothing){
 	system("zip -m -j -q ~/Documents/FolderProses1/KompresProses1.zip ~/Documents/FolderProses1/SimpanProses1.txt");
 }
-void* compress2(void *arg){
+void* compress2(void *nothing){
 	system("zip -m -j -q ~/Documents/FolderProses2/KompresProses2.zip ~/Documents/FolderProses2/SimpanProses2.txt");
 }
 
-void* extract1(void *arg){
+void* extract1(void *nothing){
 	system("unzip -q ~/Documents/FolderProses1/KompresProses1.zip -d ~/Documents/FolderProses1");
 }
-void* extract2(void *arg){
+void* extract2(void *nothing){
 	system("unzip -q ~/Documents/FolderProses2/KompresProses2.zip -d ~/Documents/FolderProses2");
 }
 
 int main(){
-	int* file1 = (int*)1;
-	int* file2 = (int*)2;
 
-	pthread_create(&tid[0], NULL, &makeDir1, (void*)&file1);
-	pthread_create(&tid[1], NULL, &makeDir2, (void*)&file2);
+    pthread_create(&tid[0], NULL, &makeDir1, NULL);
+	pthread_create(&tid[1], NULL, &makeDir2, NULL);
 	pthread_join(tid[0], NULL);
 	pthread_join(tid[1], NULL);
 
-	pthread_create(&tid[0], NULL, &storeList1, (void*)&file1);
-	pthread_create(&tid[1], NULL, &storeList2, (void*)&file2);
+	pthread_create(&tid[0], NULL, &storeList1, NULL);
+	pthread_create(&tid[1], NULL, &storeList2, NULL);
 	pthread_join(tid[0], NULL);
 	pthread_join(tid[1], NULL);
 
-	pthread_create(&tid[0], NULL, &compress1, (void*)&file1);
-	pthread_create(&tid[1], NULL, &compress2, (void*)&file2);
+	pthread_create(&tid[0], NULL, &compress1, NULL);
+	pthread_create(&tid[1], NULL, &compress2, NULL);
 	pthread_join(tid[0], NULL);
 	pthread_join(tid[1], NULL);
 
 	printf("Menunggu 15 detik untuk mengekstrak kembali\n");
-	for (int i=1; i<=15; i++){
-	printf("%d\n", i);
-	sleep(1);
-	}
+    for (int i=1; i<=15; i++){
+        printf("%d\n", i);
+        sleep(1);
+    }
 
-	pthread_create(&tid[0], NULL, &extract1, (void*)&file1);
-	pthread_create(&tid[1], NULL, &extract2, (void*)&file2);
+	pthread_create(&tid[0], NULL, &extract1, NULL);
+	pthread_create(&tid[1], NULL, &extract2, NULL);
 	pthread_join(tid[0], NULL);
 	pthread_join(tid[1], NULL);
 
